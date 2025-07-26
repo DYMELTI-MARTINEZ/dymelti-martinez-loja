6ae90d7903c0393ffb468f1d64cfb711e5ee490b
DYMELTI-MARTINEZ <alefeguimelque422@gmail.com>
2025-07-26 18:26:04 -0300

    Criar src/components/Marquee.jsx

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Marquee.css';
+
+function Marquee() {
+  return (
+    <div className="marquee">
+      <div className="track">
+        <span>DYMELTI MARTINEZ • DYMELTI MARTINEZ • DYMELTI MARTINEZ • DYMELTI MARTINEZ • DYMELTI MARTINEZ • DYMELTI MARTINEZ • DYMELTI MARTINEZ • </span>
+      </div>
+    </div>
+  );
+}
+
+export default Marquee;
